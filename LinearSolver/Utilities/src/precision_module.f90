9e0e47f3616ca536413fd9c4f1081fe8ba666e8c
Aristotelis <aristotelis.liakatas@gmail.com>
2021-09-24 15:20:38 +0100

    Adds precision control module.

---
+++ LinearSolver/Utilities/src/precision_module.f90
@@ -0,0 +1,9 @@
+
+    module custom_precision
+    
+    implicit none
+    
+    integer, parameter      :: SP = kind(1.)
+    integer, parameter      :: DP = kind(1.d0)
+    
+    end module custom_precision
