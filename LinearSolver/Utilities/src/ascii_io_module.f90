4444831138cc9cee58b3389658cefc6145395e60
Aristotelis <aristotelis.liakatas@gmail.com>
2021-09-24 22:41:05 +0100

    Adds procedures to read from ascii files.

--- LinearSolver/Utilities/src/ascii_io_module.f90
+++ LinearSolver/Utilities/src/ascii_io_module.f90
@@ -7,9 +7,11 @@
     
     private
     
+    integer, parameter  :: N_HEADER_LINES = 6
     character(11)       :: float_format = '(F10.4,X)'
     integer             :: fid
-    integer             :: irow, icol
+    integer             :: irow, icol, i
+    character(13)       :: dummy_txt
     
     interface export_to_ascii
     procedure export_to_ascii_sp
@@ -17,7 +19,20 @@
     procedure export_to_ascii_int
     end interface
     
+    interface read_from_ascii_header
+    procedure read_from_ascii_header_sp
+    procedure read_from_ascii_header_dp
+    end interface
+    
+    interface read_from_ascii_data
+    procedure read_from_ascii_data_sp
+    procedure read_from_ascii_data_dp
+    procedure read_from_ascii_data_int
+    end interface 
+    
     public export_to_ascii
+    public read_from_ascii_header
+    public read_from_ascii_data
     
     contains 
     
@@ -98,5 +113,95 @@
     end do
     close(fid)
     end subroutine export_to_ascii_int
+    
+    !#############################################
+    subroutine read_from_ascii_header_sp(fname, nrows, ncols, nodata, xll, yll, dx)
+    character(*), intent(in)                :: fname
+    integer, intent(out)                    :: nrows, ncols
+    real(SP), intent(out)                   :: nodata
+    real(SP), intent(out)                   :: xll, yll, dx
+    
+    open(newunit=fid, file=fname, status='unknown')
+    
+    read(fid, *) dummy_txt, ncols
+    read(fid, *) dummy_txt, nrows
+    read(fid, *) dummy_txt, xll
+    read(fid, *) dummy_txt, yll
+    read(fid, *) dummy_txt, dx
+    read(fid, *) dummy_txt, nodata
+    close(fid)
+    end subroutine read_from_ascii_header_sp
+    
+    !#############################################
+    subroutine read_from_ascii_header_dp(fname, nrows, ncols, nodata, xll, yll, dx)
+    character(*), intent(in)                :: fname
+    integer, intent(out)                    :: nrows, ncols
+    real(DP), intent(out)                   :: nodata
+    real(DP), intent(out)                   :: xll, yll, dx
+    
+    open(newunit=fid, file=fname, status='unknown')
+    
+    read(fid, *) dummy_txt, ncols
+    read(fid, *) dummy_txt, nrows
+    read(fid, *) dummy_txt, xll
+    read(fid, *) dummy_txt, yll
+    read(fid, *) dummy_txt, dx
+    read(fid, *) dummy_txt, nodata
+    close(fid)
+    end subroutine read_from_ascii_header_dp
+    
+    !#############################################
+    subroutine read_from_ascii_data_sp(fname, d, nrows, ncols)
+    character(*), intent(in)                :: fname
+    real(SP), allocatable, intent(out)      :: d(:,:)
+    integer, intent(out)                    :: nrows, ncols
+    
+    open(newunit=fid, file=fname, status='unknown')
+    
+    do i = 1, N_HEADER_LINES
+        read(fid, *) dummy_txt
+    end do
+    
+    do irow = nrows, 1, -1
+        read(fid, *) d(irow, :)     
+    end do
+    close(fid)
+    end subroutine read_from_ascii_data_sp
+    
+    !#############################################
+    subroutine read_from_ascii_data_dp(fname, d, nrows, ncols)
+    character(*), intent(in)                :: fname
+    real(DP), allocatable, intent(out)      :: d(:,:)
+    integer, intent(out)                    :: nrows, ncols
+    
+    open(newunit=fid, file=fname, status='unknown')
+    
+    do i = 1, N_HEADER_LINES
+        read(fid, *) dummy_txt
+    end do
+    
+    do irow = nrows, 1, -1
+        read(fid, *) d(irow, :)     
+    end do
+    close(fid)
+    end subroutine read_from_ascii_data_dp
+    
+    !#############################################
+    subroutine read_from_ascii_data_int(fname, d, nrows, ncols)
+    character(*), intent(in)                :: fname
+    integer, allocatable, intent(out)       :: d(:,:)
+    integer, intent(out)                    :: nrows, ncols
+    
+    open(newunit=fid, file=fname, status='unknown')
+    
+    do i = 1, N_HEADER_LINES
+        read(fid, *) dummy_txt
+    end do
+    
+    do irow = nrows, 1, -1
+        read(fid, *) d(irow, :)     
+    end do
+    close(fid)
+    end subroutine read_from_ascii_data_int
     
     end module ascii_io
